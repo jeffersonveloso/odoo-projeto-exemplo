810f70dc8b701bf365a775d71b6d5f06a0a03428
Jefferson Veloso <jeffersonveloso_6@hotmail.com>
2020-03-31 10:56:50 -0300

    [NEW] DIRETORIO CUSTOM

---
+++ custom/meu_modulo/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    # -*- coding: utf-8 -*-
+    'name': "Meu Módulo",
+    'summary': "Esta é uma descrição simples do novo módulo que estou desenvolvendo",
+    'description': """Este campo deve ser usado para fornecer uma descrição mais completa
+                      sobre o módulo o qual estou desenvolvendo""",
+    'author': "MeuNomeCompleto",
+    'license': "AGPL-3",
+    'website': "http://www.meuwebsite.com.br",
+    'category': 'Uncategorized',
+    'version': '12.0.1.0.0', # esta é a versão padrão para se começar um módulo!
+    'depends': ['base'],
+    #'data': ['views/meu_modelo_view.xml'],
+    #'demo': ['demo.xml'],
+}
